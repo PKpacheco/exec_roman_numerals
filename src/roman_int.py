80b6ee7579f39b74321902d5c9ced1bf37f71459
PKpacheco <pkcpweb@gmail.com>
2018-02-26 14:20:35 -0300

    add travis

--- src/roman_int.py
+++ src/roman_int.py
@@ -1,19 +1,21 @@
 VALUES = {
-        'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1
-    }
+    'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1
+}
 
 
-class RomanLetterInvalid(Exception):
+class RomanLetterInvalid(Exception):  
     pass
 
 
 def get_roman_letter():
     return str(input("Input roman letter: "))
 
+
 def letter_validation(roman_letter):
     roman_letter = roman_letter.upper()
     if (roman_letter not in VALUES):
         raise RomanLetterInvalid("Please enter a valid roman letter")
+
 
 def roman_to_int(roman_letter):
     result = 0
@@ -21,13 +23,14 @@ def roman_to_int(roman_letter):
         previous_value = roman_letter[0]
     for letter in roman_letter:
         if letter in VALUES:
-            actual_value = VALUES[letter]        
+            actual_value = VALUES[letter]
         if str(previous_value) >= str(actual_value):
             result += actual_value
         else:
             result += actual_value - (2 * previous_value)
         previous_value = actual_value
     return result
+
 
 if __name__ == '__main__':
     roman_letter = get_roman_letter()
